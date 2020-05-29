366b2373406f20cfa97427c657a965cf8f154888
agnescameron <agnesfcameron@gmail.com>
2020-05-29 17:01:03 -0400

    added a basic query, need to figure out

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
+import { useQuery } from '@apollo/react-hooks';
+import gql from "graphql-tag";
 import './App.css';
 
+const GET_SHANNON = gql` {
+  allArticles {
+    edges {
+      node {
+        title
+        tags
+        image
+        _meta{
+          tags
+        } 
+      }
+    }
+  }
+ }`
+
 function App() {
-  return (
+  const { data, loading, error } = useQuery(GET_SHANNON);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+  else {
+    console.log(data)
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { data.allArticles.edges.map((edge, index) => {
+          return <p key={index}>{ edge.node.title[0].text }</p>
+        })
+      }
     </div>
   );
+  }
 }
 
 export default App;
